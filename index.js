9f1f92703a9a1fefbe79a5c9859669213fe7aea4
Joseph Kinahan <kinahanj25@gmail.com>
2020-03-19 23:09:22 +0000

    added post req endpoint for add course, enabled json in express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require ('express');
-const app = express();;
+const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: "Javascript For Dummies"},
@@ -19,6 +21,15 @@ app.get('/api/courses/:id',(req,res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('Course with given ID not found')
     else res.send(course);
+})
+
+app.post('/api/courses',(req, res) => {
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 })
 const port = process.env.PORT || 4444; //checks 4 env. variable 'port'
 app.listen(port, () => console.log(`Listening on port ${port}...`));
