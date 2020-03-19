d1f65c2d229ce3857a80445020a1e29b32ee3016
Joseph Kinahan <kinahanj25@gmail.com>
2020-03-19 22:46:06 +0000

    ++2 get request endpoints

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require ('express');
 const app = express();;
 
+const courses = [
+    { id: 1, name: "Javascript For Dummies"},
+    { id: 2, name: "HTML Basics"},
+    { id: 3, name: "Java Rules"}
+]
+
 app.get('/',(req,res) => {
     res.send('Welcome To Learn How Here');
 });
 
 app.get('/api/courses',(req,res) => {
-    res.send(["English","HTML","Java"]);
+    res.send(courses);
 })
 
-app.listen(5555, () => console.log('Listening on port 5555'));
+app.get('/api/courses/:id',(req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('Course with given ID not found')
+    else res.send(course);
+})
+const port = process.env.PORT || 4444; //checks 4 env. variable 'port'
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
